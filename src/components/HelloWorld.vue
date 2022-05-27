cd784757517fcd4d331d22db4cee3ad773568854
quang dang <truongquangdang97@gmail.com>
2022-05-27 09:56:14 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,76 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Birthday</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item,index) in data " :key="index">
+          <th>{{item.id}}</th>
+          <th>{{item.name}}</th>
+          <th>{{item.address}}</th>
+          <th>{{item.birthday}}</th>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <div>
+        <label for="">Name</label>
+        <input type="text" v-model="obj.name">
+      </div>
+      <div>
+        <label for="">Address</label>
+        <input type="text" v-model="obj.address">
+      </div>
+      <div>
+        <label for="">Birthday</label>
+        <input type="text" v-model="obj.birthday">
+      </div>
+
+      <button @click="addHuman()">Add human</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      data:[],
+      obj:{
+        name:"",
+        address:"",
+        birthday:""
+      }
+    }
+  },
+  methods:{
+    callApi(){
+      axios.get("http://localhost:8080/api/v1/humans").then((response)=>{
+        this.data=response.data
+        console.log(response.data)
+      })
+    },
+    addHuman(){
+      axios.post("http://localhost:8080/api/v1/humans",this.obj).then((response)=>{
+        console.log(response.data)
+        this.callApi()
+      })
+    }
+  },
+  created(){
+    this.callApi()
   }
 }
 </script>
